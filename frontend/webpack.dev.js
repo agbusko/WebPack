761491631fb92e8f0a178f71ee896637d0510556
Artem <agbusko@gmail.com>
2020-02-09 22:39:20 +0300

    AB: добавил html-webpack-hot-plugin для перезагрузки html

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -2,7 +2,12 @@
 const merge = require('webpack-merge'), // для слияния конфигов webpack;
       path = require('path'),
       webpack = require('webpack'),
-      common = require('./webpack.common.js');
+      common = require('./webpack.common.js'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      HtmlWebpackHotPlugin = require('html-webpack-hot-plugin'),
+      htmlHotPlugin = new HtmlWebpackHotPlugin({
+        hot: true
+      });
 
 // #2. Подключение внутренних модулей;
 module.exports = merge(common, {
@@ -16,11 +21,20 @@ module.exports = merge(common, {
         hot: true,  // чтобы сервер работал в режиме горячей замены;
         open: true, // авто-открытие в браузере;
         overlay: true,  // для удобства отображения ошибок;
+
+        before(app, server) { // для перезагрузки html;
+            htmlHotPlugin.setDevServer(server);
+        }
     },
 
     plugins: [
         new webpack.HotModuleReplacementPlugin({
             // Options...
-        })
-    ],
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: '../index.html'
+        }),
+        htmlHotPlugin
+    ]
 });
